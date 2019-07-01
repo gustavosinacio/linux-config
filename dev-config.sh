4d01ca0f4d86dd4d4c8e1b0ba365fb204c6c92ec
Gustavo <gsinacio94@gmail.com>
2019-07-01 13:39:43 -0300

    taking comments out of dev-config

--- dev-config.sh
+++ dev-config.sh
@@ -1,11 +1,11 @@
 echo "Configuring nvm ----------------------------------------------------------"
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-# echo '
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# ' >> ~/.zshrc
+echo '
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+' >> ~/.zshrc
 
 echo "Configuring yarn ---------------------------------------------------------"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
